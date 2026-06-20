c3a63ada5a39a1b52346ae6d75b40eaa2c7ddb52
TrueGoldfish <gfioravanti35@gmail.com>
2026-06-20 19:00:16 -0400

    Added movement logic

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Variable initialization
+const SPEED = 500.0
+const DASH_SPEED = 1500.0
+const ACCELERATION = 0.05
+var dashing = false
+var can_dash = true
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Takes movement inputs and gives player ice physics with a lerp
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var top_speed = Vector2.ZERO
+	
+	# Checks which speed value to use to update the player's velocity
+	if dashing:
+		top_speed = DASH_SPEED * direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		top_speed = SPEED * direction
+	velocity = velocity.lerp(top_speed, ACCELERATION)
+	
+	# Starts dash
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$DashCooldown.start()
+		$DashDuration.start()
+	
 	move_and_slide()
+	
+	
+# Ends dash
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true

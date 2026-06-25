b04c42a281c675e1c2ebb88198316045e6f63e19
TrueGoldfish <gfioravanti35@gmail.com>
2026-06-25 17:15:53 -0400

    added working main menu

---
+++ main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
